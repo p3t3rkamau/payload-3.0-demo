682c173a974e2cfaed5a7b61a09ead7af78f9589
Peter Gitau <116557037+p3t3rkamau@users.noreply.github.com>
2024-10-12 17:55:46 +0300

    Update Media.ts

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -1,7 +1,4 @@
 import type { CollectionConfig } from 'payload'
-import { handleImageKitUpload, handleImageKitDelete } from '../imagekitPlugin/index' // Adjust the import path
-import ImageKit from 'imagekit'
-
 export const Media: CollectionConfig = {
   slug: 'media',
   access: {
